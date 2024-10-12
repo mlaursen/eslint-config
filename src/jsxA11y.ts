b47a83c4f7b2cd7ec7029e7a1736701b9a6b9140
Mikkel Laursen <mlaursen03@gmail.com>
2024-10-12 11:31:56 -0400

    fix(jsx-a11y): less strict in tests

--- src/jsxA11y.ts
+++ src/jsxA11y.ts
@@ -1,7 +1,7 @@
 import { type TSESLint } from "@typescript-eslint/utils";
 import jsxA11yPlugin from "eslint-plugin-jsx-a11y";
 import globals from "globals";
-import { JSX_FILES } from "./constants";
+import { JSX_FILES, TEST_FILES } from "./constants";
 
 export const jsxA11y: TSESLint.FlatConfig.ConfigArray = [
   {
@@ -11,6 +11,19 @@ export const jsxA11y: TSESLint.FlatConfig.ConfigArray = [
       globals: {
         ...globals.browser,
       },
+    },
+    rules: {
+      // I **only** use autoFocus within dialogs which provide the correct
+      // context for screen readers.
+      "jsx-a11y/no-autofocus": "off",
+    },
+  },
+  {
+    files: TEST_FILES,
+    rules: {
+      "jsx-a11y/anchor-has-content": "off",
+      "jsx-a11y/click-events-have-key-events": "off",
+      "jsx-a11y/no-static-element-interactions": "off",
     },
   },
 ];
